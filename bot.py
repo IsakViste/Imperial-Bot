fa2dcfcae1ad0d3c8ac639a8eadc9b2187de1f0b
iviste <isakviste@gmail.com>
2017-11-15 18:38:57 +0100

    Reworked with new features (list, roles, am, amnot)

--- bot.py
+++ bot.py
@@ -13,14 +13,61 @@ async def on_ready():
     print()
     
 # Iam Commands and Info
-iamhelp = '''```COMMAND: !iam
+iamListNames = ['Game Design Student', 'Ancien']
+iamListAlias = ['GDS', 'Anc']
+
+# Iam Help menu
+def setup_ihelp():
+    h = '''```COMMAND: !i
 DESCRIPTION: Assign yourself one of the optional roles
 USAGE:
-    !iam list     - List your optional roles
-    !iam GDS      - Become a Game Design Student```'''
+    !i list
+        List your optional roles
+    !i roles
+        List possible roles
+    !iam <role>
+        Add yourself to <role>
+    !iamnot <role>
+        Remove yourself from <role>```'''
+    return h
 
-iamListNames = ['Game Design Student']
-iamListAlias = ['GDS']
+ihelp = setup_ihelp()
+
+
+
+@bot.command(pass_context=True)
+async def i(ctx):
+    command = ctx.message.content
+    argument = ''
+    try:
+        argument = command.split(' ', 1)[1]
+    except:
+        await bot.send_message(ctx.message.channel, ihelp)
+        return
+    
+    #!i list
+    if(argument == 'list'):
+        mes = await bot.send_message(ctx.message.channel, 'Showing list...')
+        ilist = 'List of your optional roles:\n'
+        found = False
+        for i in ctx.message.author.roles:
+            for j in range (len(iamListNames)):
+                if(i.name == iamListNames[j]):
+                    found = True
+                    ilist = ilist + '`' + iamListAlias[j] + '` - ' + iamListNames[j] + '\n'
+        if(not found):
+            ilist += '- none'
+        await bot.edit_message(mes, new_content=ilist)
+        return
+    
+    #!i roles
+    if(argument == 'roles'):
+        mes = await bot.send_message(ctx.message.channel, 'Showing roles...')
+        iroles = 'List of roles you can assign yourself:'
+        for i in range(len(iamListNames)):
+            iroles = iroles + '\n`' + iamListAlias[i] + '` - ' + iamListNames[i]
+        await bot.edit_message(mes, new_content=iroles)
+        return
 
 @bot.command(pass_context=True)
 async def iam(ctx):
@@ -29,31 +76,58 @@ async def iam(ctx):
     try:
         argument = command.split(' ', 1)[1]
     except:
-        await bot.send_message(ctx.message.channel, iamhelp)
-        
-    if(argument == 'list'):
-        roleList = '```List of your optional roles:\n'
-        found = False
-        for i in ctx.message.author.roles:
-            if(i.name in iamListNames):
-                found = True
-                roleList = roleList + '- ' + i.name + '\n'
-        if(not found):
-            roleList += '- none'
-        await bot.send_message(ctx.message.channel, roleList + '```')
+        await bot.send_message(ctx.message.channel, ihelp)
+        return
     
-    if(argument == iamListAlias[0]):
-        roleName = iamListNames[0]
-        role = discord.utils.get(ctx.message.server.roles, name=roleName)
-        
-        found = False
-        for i in ctx.message.author.roles:
-            if(i.name == roleName):
-                found = True
-        if(not found):
-            await bot.add_roles(ctx.message.author, role)
-            await bot.send_message(ctx.message.channel, 'You are now a ' + iamListNames[0])
-        else:
-            await bot.send_message(ctx.message.channel, 'You already are a ' + iamListNames[0])
+    #!iam <role>
+    mes = await bot.send_message(ctx.message.channel, 'Adding role...')
+    for i in range(len(iamListAlias)):
+        if(argument == iamListAlias[i]):
+            roleName = iamListNames[i]
+            role = discord.utils.get(ctx.message.server.roles, name=roleName)
+            
+            found = False
+            for r in ctx.message.author.roles:
+                if(r.name == roleName):
+                    found = True
+            if(not found):
+                await bot.add_roles(ctx.message.author, role)
+                await bot.edit_message(mes, new_content='You are now a ' + iamListNames[i])
+                return
+            else:
+                await bot.edit_message(mes, new_content='You already are a ' + iamListNames[i])
+                return
+    await bot.edit_message(mes, new_content='Role not found, `!i roles` for list of possible roles')
+    
+@bot.command(pass_context=True)
+async def iamnot(ctx):
+    command = ctx.message.content
+    argument = ''
+    try:
+        argument = command.split(' ', 1)[1]
+    except:
+        await bot.send_message(ctx.message.channel, ihelp)
+        return
+    
+    #!iamnot <role>
+    mes = await bot.send_message(ctx.message.channel, 'Removing role...')
+    for i in range(len(iamListAlias)):
+        if(argument == iamListAlias[i]):
+            roleName = iamListNames[i]
+            role = discord.utils.get(ctx.message.server.roles, name=roleName)
+            
+            found = False
+            for r in ctx.message.author.roles:
+                if(r.name == roleName):
+                    found = True
+            if(found):
+                await bot.remove_roles(ctx.message.author, role)
+                await bot.edit_message(mes, new_content='You are no more a ' + iamListNames[i])
+                return
+            else:
+                await bot.edit_message(mes, new_content='You are not a ' + iamListNames[i])
+                return
+    await bot.edit_message(mes, new_content='Role not found, `!i list` to list your roles that you can remove')
 
-bot.run(os.environ.get('BOT_TOKEN', True))
+# Run the bot
+bot.run('MzgwMzY0MjY0OTQ4NDk4NDMz.DO3hVw.3tyVwnL0PUS_3OYy3Ez1iqQXvKE')
