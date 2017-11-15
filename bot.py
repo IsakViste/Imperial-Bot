b02e7b85f4baf0a67d7c12cf4bde866878bcf0ce
iviste <isakviste@gmail.com>
2017-11-15 04:12:35 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+import logging
+
+token = 'MzgwMTM1NDY2Nzk3NjI5NDYw.DO0iNA._-vW84Ke7kw7fD3a-rvPUz4S-9I'
+
+#logging.basicConfig(level=logging.DEBUG)
+bot = commands.Bot(command_prefix='!', description='Labelis Personal Bot')
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print(bot.user.name + ' - (' + bot.user.id + ')')
+    print()
+    
+# Iam Commands and Info
+iamhelp = '''```COMMAND: !iam
+DESCRIPTION: Assign yourself one of the optional roles
+USAGE:
+    !iam list     - List your optional roles
+    !iam GDS      - Become a Game Design Student```'''
+
+iamListNames = ['Game Design Student']
+iamListAlias = ['GDS']
+
+@bot.command(pass_context=True)
+async def iam(ctx):
+    command = ctx.message.content
+    argument = ''
+    try:
+        argument = command.split(' ', 1)[1]
+    except:
+        await bot.send_message(ctx.message.channel, iamhelp)
+        
+    if(argument == 'list'):
+        roleList = '```List of your optional roles:\n'
+        found = False
+        for i in ctx.message.author.roles:
+            if(i.name in iamListNames):
+                found = True
+                roleList = roleList + '- ' + i.name + '\n'
+        if(not found):
+            roleList += '- none'
+        await bot.send_message(ctx.message.channel, roleList + '```')
+    
+    if(argument == iamListAlias[0]):
+        roleName = iamListNames[0]
+        role = discord.utils.get(ctx.message.server.roles, name=roleName)
+        
+        found = False
+        for i in ctx.message.author.roles:
+            if(i.name == roleName):
+                found = True
+        if(not found):
+            await bot.add_roles(ctx.message.author, role)
+            await bot.send_message(ctx.message.channel, 'You are now a ' + iamListNames[0])
+        else:
+            await bot.send_message(ctx.message.channel, 'You already are a ' + iamListNames[0])
+
+bot.run(token)
